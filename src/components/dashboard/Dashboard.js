d30e47fcf88acc32b2c6dacbbcba70ee296dc3dd
Martha Sharpe <marthasharpe2020@gmail.com>
2019-12-14 10:18:28 -0500

    Create Dashboard and Project list components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Notifications from './Notifications';
+import Projectlist from '../projects/Projectlist';
 
 const Dashboard = () => {
     return (
-        <div>
-            
+        <div className="dashboard container">
+            <div className="row">
+                <div className="col s12 m6">
+                    <Projectlist />
+                </div>
+                <div className="col s12 m5 offset-m1">
+                    <Notifications />
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Dashboard
+export default Dashboard;
--- src/components/projects/Projectlist.js
+++ src/components/projects/Projectlist.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Projectlist = () => {
     return (
-        <div>
-            
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Martha Sharpe</p>
+                    <p className="grey-text">Date</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Martha Sharpe</p>
+                    <p className="grey-text">Date</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Martha Sharpe</p>
+                    <p className="grey-text">Date</p>
+                </div>
+            </div>
         </div>
     )
 }
