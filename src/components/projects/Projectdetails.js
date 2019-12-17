eb7d562241ba1864ff1b0afba4febf6bff02528e
Martha Sharpe <marthasharpe2020@gmail.com>
2019-12-17 11:49:34 -0500

    Fixed bug in project details component

--- src/components/projects/Projectdetails.js
+++ src/components/projects/Projectdetails.js
@@ -1,22 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from 'redux';
 
 const Projectdetails = (props) => {
-    const id = props.match.params.id;
-
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>This is an awesome project.</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Martha Sharpe</div>
-                    <div>Date</div>
+    const { project } = props;
+    if (project) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">
+                            {project.title}
+                        </span>
+                        <p>{project.content}</p>
+                    </div>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+                        <div>Date</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        )
+    }
 }
 
-export default Projectdetails
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null;
+    return {
+        project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps, null),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(Projectdetails);
--- src/components/projects/Projectlist.js
+++ src/components/projects/Projectlist.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Projectsummary from './Projectsummary';
+import { Link } from 'react-router-dom';
 
 const Projectlist = (props) => {
     return (
         <div className="project-list section">
             { props.projects && props.projects.map(project => {
                 return (
-                    <Projectsummary project={project} key={project.id}/>
+                    <Link to={'/project/' + project.id}>
+                        <Projectsummary
+                            project={project}
+                            key={project.id}
+                        />
+                    </Link>
                 )
             })}
         </div>
